04aabde291f93a0bebbb21125adf42a569f7200b
starkov <post.starkov@gmail.com>
2014-02-12 15:27:31 +0200

    Add 'name', 'surname', 'birthday' fields to 'New user' page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212110621) do
+ActiveRecord::Schema.define(version: 20140212125047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,9 @@ ActiveRecord::Schema.define(version: 20140212110621) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name",                   default: "", null: false
+    t.string   "surname",                default: "", null: false
+    t.date     "birthday"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
