177aed051cf81901800e3ac898eea604e1137046
starkov <post.starkov@gmail.com>
2014-02-12 22:56:09 +0200

    Fix. Redirect to main page after registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,30 @@
 class ApplicationController < ActionController::Base
-	 before_action :configure_devise_permitted_parameters, if: :devise_controller?
+  after_filter :store_location
+  before_action :configure_devise_permitted_parameters, if: :devise_controller?
+  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def store_location
+    # store last url - this is needed for post-login redirect to whatever the user last visited.
+    if (request.fullpath != "/users/sign_in" &&
+        request.fullpath != "/users/sign_up" &&
+        request.fullpath != "/users/password" &&
+        request.fullpath != "/users/sign_out" &&
+        !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath 
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
+  end
 
 
-
-  	def after_sign_in_path_for(resource)
-  		edit_user_registration_path
-  	end
+  	# def after_sign_in_path_for(resource)
+  	# 	edit_user_registration_path
+  	# end
 
   	def after_sign_out_path_for(resource_or_scope)
   		request.referrer
