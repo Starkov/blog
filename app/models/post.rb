e2726eba92f17b95a0be69edf53ca7065e5748ae
starkov <post.starkov@gmail.com>
2014-02-12 23:05:14 +0200

    Fix. Validations. Post's title is min=3, User's password is min=4.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
-	validates :title, presence: true,
-                    length: { minimum: 5 }
+	validates :title, presence: true, length: { minimum: 3 }
    
 end
