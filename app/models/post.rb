dd9d63545600811f62d88852094f9139551b9cfe
starkov <post.starkov@gmail.com>
2014-02-13 11:47:24 +0200

    Fix. Validation. Text of posts is min=5

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: { minimum: 3 }
+	validates :text, presence: true, length: { minimum: 5 }
    
 end
